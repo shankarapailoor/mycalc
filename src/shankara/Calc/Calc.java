78b814654d5c82ad6e0359ca87fdfb4b482642d1
Shankara Pailoor <shankarapailoor@gmail.com>
2019-01-30 08:06:32 -0800

    adding new formats

--- src/shankara/Calc/Calc.java
+++ src/shankara/Calc/Calc.java
@@ -1,18 +1,35 @@
 package shankara.Calc;
 
 import com.cyberpointllc.stac.calculator.Calculator;
+import com.cyberpointllc.stac.calculator.CalculatorFormatter;
 import com.cyberpointllc.stac.calculator.LargeNumeralFormatter;
+import com.cyberpointllc.stac.calculator.RomanNumeralFormatter;
 import edu.utexas.stac.Cost;
 
+
+
 public class Calc {
-        public long computeExpression(String expression) {
-            System.out.println("Expression: " + expression);
-            LargeNumeralFormatter formatter = new LargeNumeralFormatter();
-            Calculator calc = new Calculator(formatter);
-            Cost.reset();
-            try {
-                calc.handleExpression(expression);
-            } catch (Exception e){}
-            return Cost.read();
+
+    enum CalcType {
+        DIGIT, ROMAN
+    }
+
+    public long computeExpression(CalcType type, String expression) {
+        CalculatorFormatter f;
+        switch (type) {
+            case DIGIT: f = new LargeNumeralFormatter();
+                break;
+            case ROMAN: f = new RomanNumeralFormatter();
+                break;
+                default:
+                    f = new RomanNumeralFormatter();
+                    break;
         }
+        Calculator calc = new Calculator(f);
+        Cost.reset();
+        try {
+            calc.handleExpression(expression);
+        } catch (Exception e){}
+        return Cost.read();
+    }
 }
--- src/shankara/Calc/Main.java
+++ src/shankara/Calc/Main.java
@@ -3,7 +3,7 @@ package shankara.Calc;
 public class Main {
     public static void main(String[] args) {
         Calc c = new Calc();
-        long cost = c.computeExpression(args[0]);
+        long cost = c.computeExpression(Calc.CalcType.ROMAN, args[0]);
         System.out.println(cost);
     }
 }
